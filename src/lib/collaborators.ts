0fa72fe0b78fda41ca357f5e38ab0b4a4aa1cfbb
Yufeng Zhao <yufeng-zhao@outlook.com>
2025-12-18 03:04:47 -0500

    Update collaborators.ts

--- src/lib/collaborators.ts
+++ src/lib/collaborators.ts
@@ -1,7 +1,93 @@
 type TCollaboratorWebsites = Record<string, string>;
 
 const CollaboratorWebsites: TCollaboratorWebsites = {
-  Someone: "https://www.google.com",
+  "Abigail Suk": "https://fab.cba.mit.edu/classes/863.25/people/AbigailSuk",
+  "Adin Rimland": "https://fab.cba.mit.edu/classes/863.25/people/AdinRimland",
+  "Aditi Ramakrishnan":
+    "https://fab.cba.mit.edu/classes/863.25/people/AditiRamakrishnan",
+  "Aijia Yao": "https://fab.cba.mit.edu/classes/863.25/people/AijiaYao",
+  "Alex Hu": "https://fab.cba.mit.edu/classes/863.25/people/AlexHu",
+  "Alexandros Gravalos":
+    "https://fab.cba.mit.edu/classes/863.25/people/AlexandrosGravalos",
+  "Allen Ho": "https://fab.cba.mit.edu/classes/863.25/people/AllenHo",
+  "Anita Lin": "https://fab.cba.mit.edu/classes/863.25/people/AnitaLin",
+  "Annie Li": "https://fab.cba.mit.edu/classes/863.25/people/AnnieLi",
+  "Avigail Gilad": "https://fab.cba.mit.edu/classes/863.25/people/AvigailGilad",
+  "Awu Chen": "https://fab.cba.mit.edu/classes/863.25/people/AwuChen",
+  "Ayah Mahmoud": "https://fab.cba.mit.edu/classes/863.25/people/AyahMahmoud",
+  "Ben Weiss": "https://fab.cba.mit.edu/classes/863.25/people/BenWeiss",
+  "Carlos Requena":
+    "https://fab.cba.mit.edu/classes/863.25/people/CarlosRequena",
+  "Carlotta Barone-MacDonald":
+    "https://fab.cba.mit.edu/classes/863.25/people/CarlottaBarone-MacDonald",
+  "Cecilia Marsicovetere Fanjul":
+    "https://fab.cba.mit.edu/classes/863.25/people/CeciliaMarsicovetereFanjul",
+  "Charles Lu": "https://fab.cba.mit.edu/classes/863.25/people/CharlesLu",
+  "Claire Hanly": "https://fab.cba.mit.edu/classes/863.25/people/ClaireHanly",
+  "Claire Wang": "https://fab.cba.mit.edu/classes/863.25/people/ClaireWang",
+  "Dimitar Dimitrov":
+    "https://fab.cba.mit.edu/classes/863.25/people/DimitarDimitrov",
+  "Edward Chen": "https://fab.cba.mit.edu/classes/863.25/people/EdwardChen",
+  "Eghosa Ohenhen":
+    "https://fab.cba.mit.edu/classes/863.25/people/EghosaOhenhen",
+  "Eitan Wolf": "https://fab.cba.mit.edu/classes/863.25/people/EitanWolf",
+  "Geoffrey Hazard":
+    "https://fab.cba.mit.edu/classes/863.25/people/GeoffreyHazard",
+  "Guillermo Mendoza Contreras":
+    "https://fab.cba.mit.edu/classes/863.25/people/GuillermoMendozaContreras",
+  "Harrison White":
+    "https://fab.cba.mit.edu/classes/863.25/people/HarrisonWhite",
+  "Hayley Bloch": "https://fab.cba.mit.edu/classes/863.25/people/HayleyBloch",
+  "Huanyu Ren": "https://fab.cba.mit.edu/classes/863.25/people/HuanyuRen",
+  "Jacqueline Orr":
+    "https://fab.cba.mit.edu/classes/863.25/people/JacquelineOrr",
+  "Jessica Shoemaker":
+    "https://fab.cba.mit.edu/classes/863.25/people/JessicaShoemaker",
+  "Jing Xuan Sun": "https://fab.cba.mit.edu/classes/863.25/people/JingXuanSun",
+  "Justin Wan": "https://fab.cba.mit.edu/classes/863.25/people/JustinWan",
+  "Jyo Pari": "https://fab.cba.mit.edu/classes/863.25/people/JyoPari",
+  "Katherine Yan": "https://fab.cba.mit.edu/classes/863.25/people/KatherineYan",
+  "Katrina Li": "https://fab.cba.mit.edu/classes/863.25/people/KatrinaLi",
+  "Kevin Wang": "https://fab.cba.mit.edu/classes/863.25/people/KevinWang",
+  "Mariam Fitaihi":
+    "https://fab.cba.mit.edu/classes/863.25/people/MariamFitaihi",
+  "Matti Gruener": "https://fab.cba.mit.edu/classes/863.25/people/MattiGruener",
+  "Maya Jyothinagaram":
+    "https://fab.cba.mit.edu/classes/863.25/people/MayaJyothinagaram",
+  "Miranda Li": "https://fab.cba.mit.edu/classes/863.25/people/MirandaLi",
+  "Qilmeg Doudatcz":
+    "https://fab.cba.mit.edu/classes/863.25/people/QilmegDoudatcz",
+  "Ray Song": "https://fab.cba.mit.edu/classes/863.25/people/RaySong",
+  "Rodmehr Basidj":
+    "https://fab.cba.mit.edu/classes/863.25/people/RodmehrBasidj",
+  "Rodrigo Gallardo":
+    "https://fab.cba.mit.edu/classes/863.25/people/RodrigoGallardo",
+  "Romeo Giudici": "https://fab.cba.mit.edu/classes/863.25/people/RomeoGiudici",
+  "Ruipeng Wang": "https://fab.cba.mit.edu/classes/863.25/people/RuipengWang",
+  "Ryan Hoffman": "https://fab.cba.mit.edu/classes/863.25/people/RyanHoffman",
+  "Saetbyeol Leeyouk":
+    "https://fab.cba.mit.edu/classes/863.25/people/SaetbyeolLeeyouk",
+  "Saleem Aldajani":
+    "https://fab.cba.mit.edu/classes/863.25/people/SaleemAldajani",
+  "Sara Fernandez":
+    "https://fab.cba.mit.edu/classes/863.25/people/SaraFernandez",
+  "Shengtao Shen": "https://fab.cba.mit.edu/classes/863.25/people/ShengtaoShen",
+  "Sophia Chefalo":
+    "https://fab.cba.mit.edu/classes/863.25/people/SophiaChefalo",
+  "Srikanth Nadhamuni":
+    "https://fab.cba.mit.edu/classes/863.25/people/SrikanthNadhamuni",
+  "Stasya Selizhuk":
+    "https://fab.cba.mit.edu/classes/863.25/people/StasyaSelizhuk",
+  "Sun Chuanqi": "https://fab.cba.mit.edu/classes/863.25/people/SunChuanqi",
+  "Taeija-Lee Hall-Watts":
+    "https://fab.cba.mit.edu/classes/863.25/people/Taeija-LeeHall-Watts",
+  "Tushar Kanade": "https://fab.cba.mit.edu/classes/863.25/people/TusharKanade",
+  "Tyler Barron": "https://fab.cba.mit.edu/classes/863.25/people/TylerBarron",
+  "Tyler Jensen Hill":
+    "https://fab.cba.mit.edu/classes/863.25/people/TylerJensenHill",
+  "Vittorio Tosi": "https://fab.cba.mit.edu/classes/863.25/people/VittorioTosi",
+  "Yu Wang": "https://fab.cba.mit.edu/classes/863.25/people/YuWang",
+  "Yufeng Zhao": "https://fab.cba.mit.edu/classes/863.25/people/YufengZhao",
 };
 
 export function getCollaboratorWebsite(name: string): string | undefined {
